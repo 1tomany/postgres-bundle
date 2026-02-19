e0313abfc82cc66bf1e1f8b760c2d2ed597ce00b
Vic Cherubini <vcherubini@gmail.com>
2026-02-19 10:37:20 -0600

    wip

---
+++ src/Contract/Exception/ExceptionInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace OneToMany\PostgresBundle\Contract\Exception;
+
+interface ExceptionInterface extends \Throwable
+{
+}
---
+++ src/PostgresBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OneToMany\PostgresBundle;
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class PostgresBundle extends AbstractBundle
+{
+    /**
+     * @see Symfony\Component\DependencyInjection\Extension\ConfigurableExtensionInterface
+     *
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+    }
+}
