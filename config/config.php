6f834544abc8cf0465d194144673df6054faec0b
Vic Cherubini <vcherubini@gmail.com>
2026-02-19 11:32:41 -0600

    wip

--- config/config.php
+++ config/config.php
@@ -7,18 +7,25 @@ use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
  * @param DefinitionConfigurator<'array'> $configurator
  */
 $configurator = static function (DefinitionConfigurator $configurator): void {
-    $middleware = new ArrayNodeDefinition('middleware')
-        ->children()
-            ->stringNode('time_zone')
-                ->cannotBeEmpty()
-                ->defaultValue('UTC')
-            ->end()
-        ->end();
+    // $middleware = new ArrayNodeDefinition('middleware')
+    //     ->children()
+    //         ->stringNode('time_zone')
+    //             ->cannotBeEmpty()
+    //             ->defaultValue('UTC')
+    //         ->end()
+    //     ->end();
 
     $configurator
         ->rootNode()
             ->children()
-                ->append($middleware)
+                ->arrayNode('middleware')
+                    ->children()
+                        ->stringNode('time_zone')
+                            ->cannotBeEmpty()
+                            ->defaultValue('UTC')
+                        ->end()
+                    ->end()
+                ->end()
             ->end()
         ->end();
 };
