fcda7ee948842e38c3ca8c094990ae444cc851f9
Vic Cherubini <vcherubini@gmail.com>
2026-02-19 10:24:29 -0600

    wip

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,42 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfig;
+
+$finder = new Finder();
+
+$finder->in([
+    './',
+    './config/',
+    './src/',
+    './tests/',
+]);
+
+$finder->exclude([
+    'vendor',
+]);
+
+$config = new Config()->setParallelConfig(...[
+    'config' => new ParallelConfig(4),
+]);
+
+$config->setFinder($finder);
+$config->setRules([
+    '@Symfony' => true,
+    'global_namespace_import' => [
+        'import_classes' => false,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
+    'phpdoc_align' => [
+        'align' => 'left',
+    ],
+    'phpdoc_to_comment' => [
+        'ignored_tags' => [
+            'disregard',
+        ],
+    ],
+]);
+
+return $config;
