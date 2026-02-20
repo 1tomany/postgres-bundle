444d0601fe97ea3a911c6db7bf2b5b442b7cb232
Vic Cherubini <vcherubini@gmail.com>
2026-02-20 13:12:26 -0600

    wip

--- src/Driver/AdvisoryLockManager.php
+++ src/Driver/AdvisoryLockManager.php
@@ -9,53 +9,55 @@ use OneToMany\PostgresBundle\Exception\RuntimeException;
 
 use function sprintf;
 
-final readonly class AdvisoryLockManager
+class AdvisoryLockManager
 {
-    public function __construct(private Connection $connection)
+    public function __construct(private ?Connection $connection = null)
     {
     }
 
     public function lock(int $lockKey): void
     {
-        $this->assertLockIdIsPositive($lockKey);
-        $this->assertDatabaseConnectionExists();
+        $this->assertLockKeyIsPositive($lockKey);
 
         try {
-            $this->connection->executeStatement('SELECT pg_advisory_lock(?)', [$lockKey]);
+            $this->getConnection()->executeStatement('SELECT pg_advisory_lock(?)', [$lockKey]);
         } catch (DbalExceptionInterface $e) {
-            throw new RuntimeException(sprintf('Acquiring advisory lock %d failed.', $lockKey), previous: $e);
+            throw new RuntimeException(sprintf('Acquiring the advisory lock %d failed.', $lockKey), previous: $e);
         }
     }
 
     public function unlock(int $lockKey): void
     {
-        $this->assertLockIdIsPositive($lockKey);
-        $this->assertDatabaseConnectionExists();
+        $this->assertLockKeyIsPositive($lockKey);
 
         try {
-            $this->connection->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
+            $this->getConnection()->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
         } catch (DbalExceptionInterface $e) {
-            throw new RuntimeException(sprintf('Releasing advisory lock %d failed.', $lockKey), previous: $e);
+            throw new RuntimeException(sprintf('Releasing the advisory lock %d failed.', $lockKey), previous: $e);
         }
+    }
+
+    public function getConnection(): Connection
+    {
+        return $this->connection ?? throw new RuntimeException('The advisory lock failed because the DBAL Connection object is null.');
+    }
+
+    public function setConnection(Connection $connection): static
+    {
+        $this->connection = $connection;
+
+        return $this;
     }
 
     /**
      * @throws InvalidArgumentException the `$lockKey` is less than one
      */
-    private function assertLockIdIsPositive(int $lockKey): void
+    private function assertLockKeyIsPositive(int $lockKey): bool
     {
         if ($lockKey < 1) {
             throw new InvalidArgumentException('The advisory lock key must be greater than zero.');
         }
-    }
 
-    /**
-     * @throws RuntimeException a database connection does not exist
-     */
-    private function assertDatabaseConnectionExists(): void
-    {
-        if (!$this->connection->isConnected()) {
-            throw new RuntimeException('The advisory lock failed because a connection to the database does not exist.');
-        }
+        return true;
     }
 }
