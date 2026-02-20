71c060d9f4cc9cd9715edda63c5ad25f3c5976f4
Vic Cherubini <vcherubini@gmail.com>
2026-02-20 12:33:52 -0600

    wip

--- config/services.php
+++ config/services.php
@@ -4,12 +4,15 @@ use OneToMany\PostgresBundle\Driver\AdvisoryLockManager;
 use OneToMany\PostgresBundle\Middleware\SetTimeZoneMiddleware;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
 return static function (ContainerConfigurator $container): void {
     $container
         ->services()
 
             // Drivers
             ->set('1tomany.postgres_bundle.driver.advisory_lock_manager', AdvisoryLockManager::class)
+            ->alias(AdvisoryLockManager::class, service('1tomany.postgres_bundle.driver.advisory_lock_manager'))
 
             // Middlewares
             ->set('1tomany.postgres_bundle.middleware.set_time_zone', SetTimeZoneMiddleware::class)
