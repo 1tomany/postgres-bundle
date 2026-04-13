f748c8f88e9a8d397bef91c1f0dfade052f0ecf4
Vic Cherubini <vcherubini@gmail.com>
2026-04-13 16:57:34 -0500

    wip

---
+++ src/Command/BackupDatabaseCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OneToMany\PostgresBundle\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'onetomany:postgres:backup-database',
+    description: 'backups a database',
+)]
+final readonly class BackupDatabaseCommand
+{
+    public function __invoke(
+        SymfonyStyle $io,
+    ): int
+    {
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @see Symfony\Component\Console\Command\Command
+     */
+    /*
+    protected function configure(): void
+    {
+        $this
+            ->setName('onetomany:postgres:backup-database')
+            ->setDescription('Lists all available models by vendor');
+    }
+    */
+}
