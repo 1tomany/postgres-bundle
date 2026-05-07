a6ee6e14fe152aecfb68ef52fae8aff73a0115d1
Vic Cherubini <vcherubini@gmail.com>
2026-05-07 18:16:42 -0500

    wip

--- src/Driver/AdvisoryLockManager.php
+++ src/Driver/AdvisoryLockManager.php
@@ -11,12 +11,20 @@ use function sprintf;
 
 class AdvisoryLockManager
 {
+    /**
+     * @var array<positive-int, true>
+     */
+    private array $lockKeys = [];
+
     public function __construct(
         private ?Connection $connection = null,
     )
     {
     }
 
+    /**
+     * @throws RuntimeException when acquiring the advisory lock fails
+     */
     public function lock(int $lockKey): void
     {
         $this->assertLockKeyIsPositive($lockKey);
@@ -26,22 +34,34 @@ class AdvisoryLockManager
         } catch (DbalExceptionInterface $e) {
             throw new RuntimeException(sprintf('Acquiring advisory lock %d failed.', $lockKey), previous: $e);
         }
+
+        $this->lockKeys[$lockKey] = true;
     }
 
+    /**
+     * @throws RuntimeException when releasing the advisory lock fails
+     */
     public function unlock(int $lockKey): void
     {
         $this->assertLockKeyIsPositive($lockKey);
 
-        try {
-            $this->getConnection()->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
-        } catch (DbalExceptionInterface $e) {
-            throw new RuntimeException(sprintf('Releasing advisory lock %d failed.', $lockKey), previous: $e);
+        if (\array_key_exists($lockKey, $this->lockKeys)) {
+            try {
+                $this->getConnection()->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
+            } catch (DbalExceptionInterface $e) {
+                throw new RuntimeException(sprintf('Releasing advisory lock %d failed.', $lockKey), previous: $e);
+            }
+
+            unset($this->lockKeys[$lockKey]);
         }
     }
 
+    /**
+     * @throws RuntimeException when a connection to the database could not be established
+     */
     public function getConnection(): Connection
     {
-        return $this->connection ?? throw new RuntimeException('Accessing the advisory locker failed because a connection with the database could not be established.');
+        return $this->connection ?? throw new RuntimeException('A connection with the database could not be established.');
     }
 
     public function setConnection(Connection $connection): static
@@ -52,12 +72,14 @@ class AdvisoryLockManager
     }
 
     /**
-     * @throws InvalidArgumentException the `$lockKey` is less than one
+     * @phpstan-assert positive-int $lockKey
+     *
+     * @throws InvalidArgumentException when the lock key is not a positive integer
      */
     private function assertLockKeyIsPositive(int $lockKey): bool
     {
         if ($lockKey < 1) {
-            throw new InvalidArgumentException('The advisory lock key must be greater than zero.');
+            throw new InvalidArgumentException('The lock key must be a positive integer.');
         }
 
         return true;
