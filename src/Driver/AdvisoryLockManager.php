18758edb542455bd9a7d435a67ffa2f6a992ead0
Vic Cherubini <vcherubini@gmail.com>
2026-05-07 20:21:12 -0500

    [#7] Track active advisory locks to avoid unlocking an inactive lock (#8)
    resolves #7

--- src/Driver/AdvisoryLockManager.php
+++ src/Driver/AdvisoryLockManager.php
@@ -3,7 +3,7 @@
 namespace OneToMany\PostgresBundle\Driver;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception as DbalExceptionInterface;
+use Doctrine\DBAL\Exception as DoctrineExceptionInterface;
 use OneToMany\PostgresBundle\Exception\InvalidArgumentException;
 use OneToMany\PostgresBundle\Exception\RuntimeException;
 
@@ -11,35 +11,56 @@ use function sprintf;
 
 class AdvisoryLockManager
 {
-    public function __construct(private ?Connection $connection = null)
-    {
+    /**
+     * @var array<positive-int, true>
+     */
+    private array $lockKeys = [];
+
+    public function __construct(
+        private ?Connection $connection = null,
+    ) {
     }
 
+    /**
+     * @throws RuntimeException when acquiring the advisory lock fails
+     */
     public function lock(int $lockKey): void
     {
         $this->assertLockKeyIsPositive($lockKey);
 
         try {
             $this->getConnection()->executeStatement('SELECT pg_advisory_lock(?)', [$lockKey]);
-        } catch (DbalExceptionInterface $e) {
-            throw new RuntimeException(sprintf('Acquiring advisory lock %d failed.', $lockKey), previous: $e);
+        } catch (DoctrineExceptionInterface $e) {
+            throw new RuntimeException(sprintf('Acquiring advisory lock "%d" failed.', $lockKey), previous: $e);
         }
+
+        $this->lockKeys[$lockKey] = true;
     }
 
+    /**
+     * @throws RuntimeException when releasing the advisory lock fails
+     */
     public function unlock(int $lockKey): void
     {
         $this->assertLockKeyIsPositive($lockKey);
 
-        try {
-            $this->getConnection()->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
-        } catch (DbalExceptionInterface $e) {
-            throw new RuntimeException(sprintf('Releasing advisory lock %d failed.', $lockKey), previous: $e);
+        if (isset($this->lockKeys[$lockKey])) {
+            try {
+                $this->getConnection()->executeStatement('SELECT pg_advisory_unlock(?)', [$lockKey]);
+            } catch (DoctrineExceptionInterface $e) {
+                throw new RuntimeException(sprintf('Releasing advisory lock "%d" failed.', $lockKey), previous: $e);
+            }
+
+            unset($this->lockKeys[$lockKey]);
         }
     }
 
+    /**
+     * @throws RuntimeException when a database connection is not found
+     */
     public function getConnection(): Connection
     {
-        return $this->connection ?? throw new RuntimeException('Accessing the advisory locker failed because a connection with the database could not be established.');
+        return $this->connection ?? throw new RuntimeException('Database connection not found.');
     }
 
     public function setConnection(Connection $connection): static
@@ -50,12 +71,14 @@ class AdvisoryLockManager
     }
 
     /**
-     * @throws InvalidArgumentException the `$lockKey` is less than one
+     * @phpstan-assert positive-int $lockKey
+     *
+     * @throws InvalidArgumentException when the lock key is not a positive integer
      */
     private function assertLockKeyIsPositive(int $lockKey): bool
     {
         if ($lockKey < 1) {
-            throw new InvalidArgumentException('The advisory lock key must be greater than zero.');
+            throw new InvalidArgumentException('The lock key must be a positive integer.');
         }
 
         return true;
