8c32eb9b6765c31fd3a3898f936d993458d43e99
Vic Cherubini <vcherubini@gmail.com>
2026-09-09 11:32:54 -0500

    [#13] Avoid querying the database to see if an advisory block exists (#15)
    resolves #13

--- src/Driver/AdvisoryLockManager.php
+++ src/Driver/AdvisoryLockManager.php
@@ -23,7 +23,7 @@ class AdvisoryLockManager
 
     public function exists(int|string $lockKey): bool
     {
-        $key = $this->generateKey($lockKey);
+        $key = $this->hashKey($lockKey);
 
         if (isset($this->locks[$key])) {
             return $this->locks[$key];
@@ -38,7 +38,7 @@ class AdvisoryLockManager
     public function lock(int|string $lockKey): void
     {
         if (!$this->exists($lockKey)) {
-            $key = $this->generateKey($lockKey);
+            $key = $this->hashKey($lockKey);
 
             try {
                 $this->getConnection()->executeStatement(sprintf('SELECT pg_advisory_lock(%d)', $key));
@@ -56,7 +56,7 @@ class AdvisoryLockManager
     public function unlock(int|string $lockKey): void
     {
         if ($this->exists($lockKey)) {
-            $key = $this->generateKey($lockKey);
+            $key = $this->hashKey($lockKey);
 
             try {
                 $this->getConnection()->executeStatement(sprintf('SELECT pg_advisory_unlock(%d)', $key));
@@ -83,7 +83,7 @@ class AdvisoryLockManager
         return $this;
     }
 
-    private function generateKey(int|string $lockKey): int
+    private function hashKey(int|string $lockKey): int
     {
         if (is_string($lockKey)) {
             return crc32($lockKey);
